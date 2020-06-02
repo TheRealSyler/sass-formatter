60ead08c71c519f15f56ea505da72f8156113ad0
syler <hileo77@gmail.com>
2020-06-02 15:08:21 +0200

    update suf-regex (add figcaption tag support) close #24

--- src/test/html.spec.ts
+++ src/test/html.spec.ts
@@ -3,3 +3,8 @@ import { SassFormatter as SF } from '../index';
 test('Sass Format: path tag', () => {
   expect(SF.Format('.class\n  path\n  margin: 20px')).toBe('.class\n  path\n    margin: 20px\n');
 });
+test('Sass Format: figcaption tag', () => {
+  expect(SF.Format('.class\n  figcaption\n  margin: 20px')).toBe(
+    '.class\n  figcaption\n    margin: 20px\n'
+  );
+});
